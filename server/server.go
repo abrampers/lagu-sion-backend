cab2fd81ad18f983e4c361951f8fe05e39644d04
Abram Situmorang <abram.perdanaputra@gmail.com>
2020-07-09 18:56:59 +0700

    Implemented null for grpc stubs

--- server/server.go
+++ server/server.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"context"
 	pb "github.com/abrampers/lagu-sion-backend/lagusion"
+	empty "github.com/golang/protobuf/ptypes/empty"
 )
 
-type routeGuideServer struct {
+type laguSionServer struct {
 	pb.UnimplementedLaguSionServiceServer
+}
+
+func (s *laguSionServer) ListAllSongs(ctx context.Context, empty *empty.Empty) (*pb.ListSongResponse, error) {
+	return &pb.ListSongResponse{}, nil
+}
+
+func (s *laguSionServer) ListSongs(ctx context.Context, ad *pb.ListSongsRequest) (*pb.ListSongResponse, error) {
+	return &pb.ListSongResponse{}, nil
 }
 
 func main() {
